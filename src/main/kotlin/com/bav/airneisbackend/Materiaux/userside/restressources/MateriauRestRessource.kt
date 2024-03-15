44fce366fc3e7a2f6078c58a7ac889350721c23d
Benoit <fardouxbenoit@gmail.com>
2024-03-15 10:53:49 +0100

    deletion(materiau) : mise a jour de la classe materiau

--- src/main/kotlin/com/bav/airneisbackend/Materiaux/userside/restressources/MateriauRestRessource.kt
+++ src/main/kotlin/com/bav/airneisbackend/Materiaux/userside/restressources/MateriauRestRessource.kt
@@ -1,5 +1,7 @@
 package com.bav.airneisbackend.Materiaux.userside.restressources
 data class MateriauRestRessource (
     val id : String,
-    val nom : String
+    val nom : String,
+    val type :String,
+    val image : String
 )
