66aad4f5b2ee41ce086be1392f0a432450533920
Benoit <fardouxbenoit@gmail.com>
2024-04-05 08:42:39 +0200

    feat(produit) : ajout de l'id en nullable

--- src/test/kotlin/com/bav/airneisbackend/Materiaux/fixture/MateriauFixture.kt
+++ src/test/kotlin/com/bav/airneisbackend/Materiaux/fixture/MateriauFixture.kt
@@ -2,11 +2,10 @@ package com.bav.airneisbackend.Materiaux.fixture
 
 import com.bav.airneisbackend.Materiaux.domain.model.Materiau
 import com.bav.airneisbackend.Materiaux.serverside.dto.MateriauDocument
-import org.bson.types.ObjectId
 
 class MateriauFixture {
     companion object {
-        val materiauDocument = MateriauDocument(id = ObjectId(), nom = "chene", type = "bois", image = "chene.jpg")
-        val materiau = Materiau(id = ObjectId(), nom = "chene", type = "bois", image = "chene.jpg")
+        val materiauDocument = MateriauDocument(id = "", nom = "chene", type = "bois", image = "chene.jpg")
+        val materiau = Materiau(id = "", nom = "chene", type = "bois", image = "chene.jpg")
     }
 }
