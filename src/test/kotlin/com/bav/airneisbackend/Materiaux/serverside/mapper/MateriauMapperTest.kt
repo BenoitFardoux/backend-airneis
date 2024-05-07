e0e548fc77dd63715218947146e5c96132eec6ee
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-07 12:41:30 +0200

    feat(materiau-par-id): ajout de la récupération par id

--- src/test/kotlin/com/bav/airneisbackend/Materiaux/serverside/mapper/MateriauMapperTest.kt
+++ src/test/kotlin/com/bav/airneisbackend/Materiaux/serverside/mapper/MateriauMapperTest.kt
@@ -2,6 +2,7 @@ package com.bav.airneisbackend.Materiaux.serverside.mapper
 
 import com.bav.airneisbackend.Materiaux.fixture.MateriauFixture
 import com.bav.airneisbackend.Materiaux.serverside.mapper.MateriauMapper.toMateriau
+import com.bav.airneisbackend.Materiaux.serverside.mapper.MateriauMapper.toMateriauDocument
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -15,5 +16,26 @@ class MateriauMapperTest{
         val materiau = materiauDocument.toMateriau()
         // THEN
         assertThat(materiau).usingRecursiveComparison().ignoringFields("id").isEqualTo(materiauAttendu)
+    }
+
+    @Test
+    fun `Lorsque je mappe un objet Materiau en MateriauDocument, le materiauDocument mappé contient les donnée du materiau`(){
+        // GIVEN
+        val materiau = MateriauFixture.materiau
+        val materiauDocumentAttendu = MateriauFixture.materiauDocument
+        // WHEN
+        val materiauDocument = materiau.toMateriauDocument()
+        // THEN
+        assertThat(materiauDocument).usingRecursiveComparison().isEqualTo(materiauDocumentAttendu)
+    }
+
+    @Test
+    fun `lorsque je mappe un objet Materiau avec un id vide, le materiauDocument mappé contient un id généré`(){
+        // GIVEN
+        val materiau = MateriauFixture.materiau.copy(id = "")
+        // WHEN
+        val materiauDocument = materiau.toMateriauDocument()
+        // THEN
+        assertThat(materiauDocument.id).isNotEmpty()
     }
 }
