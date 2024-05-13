0a506b17af433a3b5ce2b525cfcf4f482820f8b4
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-13 13:58:22 +0200

    feat(produit): ajout de la recherche

--- src/main/kotlin/com/bav/airneisbackend/produit/domain/port/serverside/categorie/PourRecupererCategoriesServerSidePort.kt
+++
@@ -1,9 +0,0 @@
-package com.bav.airneisbackend.produit.domain.port.serverside.categorie
-
-import com.bav.airneisbackend.produit.domain.model.Categorie
-
-interface PourRecupererCategoriesServerSidePort {
-    fun recupererCategories() : List<Categorie>
-
-    fun recupererCategorieParId(id: String) : Categorie
-}
--- src/main/kotlin/com/bav/airneisbackend/produit/serverside/adapter/mongodb/repository/MongoDbCategorieRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bav.airneisbackend.produit.serverside.adapter.mongodb.repository
-
-import com.bav.airneisbackend.produit.serverside.dto.CategorieDocument
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface MongoDbCategorieRepository: MongoRepository<CategorieDocument, String>
