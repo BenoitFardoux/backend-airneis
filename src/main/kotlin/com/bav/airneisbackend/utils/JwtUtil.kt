8b7d8b5bf4c47a56e573e4efea3013b5c9887d98
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2024-05-20 14:23:44 +0200

    feat(security):ajout du fichier utilitaire pour les JWT

---
+++ src/main/kotlin/com/bav/airneisbackend/utils/JwtUtil.kt
@@ -0,0 +1,57 @@
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+
+@Component
+class JwtUtil {
+
+    private val secretKey: SecretKey =   Jwts.SIG.HS256.key().build();
+
+    fun extractUsername(token: String): String {
+        return extractClaim(token) { it.subject }
+    }
+
+    fun extractExpiration(token: String): Date {
+        return extractClaim(token) { it.expiration }
+    }
+
+    fun <T> extractClaim(token: String, claimsResolver: (Claims) -> T): T {
+        val claims = extractAllClaims(token)
+        return claimsResolver(claims)
+    }
+
+    private fun extractAllClaims(token: String): Claims {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+    }
+
+    private fun isTokenExpired(token: String): Boolean {
+        return extractExpiration(token).before(Date())
+    }
+
+    fun generateToken(username: String): String {
+        val claims: Map<String, Any> = HashMap()
+        return createToken(claims, username)
+    }
+
+    private fun createToken(claims: Map<String, Any>, subject: String): String {
+        return Jwts.builder()
+            .claims(claims)
+            .subject(subject)
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+            .signWith(secretKey)
+            .compact()
+    }
+
+    fun validateToken(token: String, username: String): Boolean {
+        val tokenUsername = extractUsername(token)
+        return (tokenUsername == username && !isTokenExpired(token))
+    }
+}
