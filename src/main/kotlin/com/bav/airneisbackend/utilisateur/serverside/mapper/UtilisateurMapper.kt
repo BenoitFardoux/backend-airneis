9cb7bcf97016bde1352c77ae88dbc46ecfd9dc65
benoit.fardoux <benoit.fardoux@rtssolutions.fr>
2024-05-23 00:23:16 +0200

    feat(sign in): ajout mapper

---
+++ src/main/kotlin/com/bav/airneisbackend/utilisateur/serverside/mapper/UtilisateurMapper.kt
@@ -0,0 +1,39 @@
+package com.bav.airneisbackend.utilisateur.serverside.mapper
+
+import com.bav.airneisbackend.utilisateur.domain.model.Utilisateur
+import com.bav.airneisbackend.utilisateur.serverside.dto.UtilisateurDocument
+import org.bson.types.ObjectId
+
+object UtilisateurMapper {
+    fun UtilisateurDocument.toUtilisateur() : Utilisateur {
+        return Utilisateur(
+            id = id.ifBlank { ObjectId().toHexString()  },
+            nom = nom,
+            prenom = prenom,
+            email = email,
+            motDePasse = motDePasse,
+            adresse = adresse,
+            numeroDeTelephone = numeroDeTelephone,
+            paiements = paiements,
+            panierActuel = panierActuel,
+            commandes = commandes,
+            verifie = verifie
+        )
+    }
+
+    fun Utilisateur.toUtilisateurDocument() : UtilisateurDocument {
+        return UtilisateurDocument(
+            id = id,
+            nom = nom,
+            prenom = prenom,
+            email = email,
+            motDePasse = motDePasse,
+            adresse = adresse,
+            numeroDeTelephone = numeroDeTelephone,
+            paiements = paiements,
+            panierActuel = panierActuel,
+            commandes = commandes,
+            verifie = verifie
+        )
+    }
+}
