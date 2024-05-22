266a867bc561c8d98c9faf66406649475a67d61c
valentin.leroux <Valentin.leroux@rtssolutions.fr>
2024-05-22 16:40:10 +0200

    feat(categorie): Ajout serverside

--- src/main/kotlin/com/bav/airneisbackend/categorie/serverside/adapter/repository/PourRecupererCategorieRepository.kt
+++ src/main/kotlin/com/bav/airneisbackend/categorie/serverside/adapter/repository/PourRecupererCategorieRepository.kt
@@ -1,9 +1,32 @@
 package com.bav.airneisbackend.categorie.serverside.adapter.repository
 
+import com.bav.airneisbackend.categorie.domain.model.Categorie
+import com.bav.airneisbackend.categorie.domain.model.Produit
 import com.bav.airneisbackend.categorie.domain.port.serverside.categorie.PourRecupererCategories
+import com.bav.airneisbackend.categorie.serverside.adapter.mongodb.repository.MongoDbCategorieRepository
+import com.bav.airneisbackend.categorie.serverside.mapper.CategorieMapper.toCategorie
+import com.bav.airneisbackend.produit.serverside.adapter.mongodb.repository.MongoDbProduitRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 
 
 @Repository
-class PourRecupererCategorieRepository : PourRecupererCategories{
+class PourRecupererCategorieRepository(
+    val categorieRepository: MongoDbCategorieRepository,
+    val produitRepository: MongoDbProduitRepository
+)
+  : PourRecupererCategories{
+  override fun invoke(pageable: Pageable): Page<Categorie> {
+    val categoriesRecupere = categorieRepository.findAll(pageable)
+    return categoriesRecupere.map {
+      it.toCategorie(it.produit.map {
+        produitDocument -> Produit(
+          produitDocument.id,
+          produitRepository.findById(produitDocument.id).get().nom
+        )
+      })
+    }
+  }
+
 }
