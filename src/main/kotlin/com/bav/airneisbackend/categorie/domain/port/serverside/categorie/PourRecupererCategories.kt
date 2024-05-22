69f7da082362489b6ba2420e31613f62e08394ce
valentin.leroux <Valentin.leroux@rtssolutions.fr>
2024-05-22 16:40:10 +0200

    feat(categorie): Ajout serverside

--- src/main/kotlin/com/bav/airneisbackend/categorie/domain/port/serverside/categorie/PourRecupererCategories.kt
+++ src/main/kotlin/com/bav/airneisbackend/categorie/domain/port/serverside/categorie/PourRecupererCategories.kt
@@ -1,4 +1,9 @@
 package com.bav.airneisbackend.categorie.domain.port.serverside.categorie
 
-interface PourRecupererCategories {
+import com.bav.airneisbackend.categorie.domain.model.Categorie
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+fun interface PourRecupererCategories {
+  operator fun invoke(pageable: Pageable): Page<Categorie>
 }
