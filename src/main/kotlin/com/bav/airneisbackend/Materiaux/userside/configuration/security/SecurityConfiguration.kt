7de74b595e8182b3a8e9046562cf5a4589077ab2
benoit.fardoux <benoit.fardoux@rtssolutions.fr>
2024-05-24 21:51:41 +0200

    feat(sign in): ajout des dtos

--- src/main/kotlin/com/bav/airneisbackend/Materiaux/userside/configuration/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/bav/airneisbackend/Materiaux/userside/configuration/security/SecurityConfiguration.kt
@@ -1,24 +1,35 @@
 package com.bav.airneisbackend.Materiaux.userside.configuration.security
 
+import com.bav.airneisbackend.config.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
-
+class SecurityConfiguration(
+    private val authenticationProvider: AuthenticationProvider,
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { csrf -> csrf.disable() }
-            .authorizeHttpRequests { authorize ->
-                authorize.requestMatchers(
-                    "/actuator/**", "/swagger-ui/**", "/v3/api-docs/**", "/**"
-                )
-                    .permitAll()
+        http.csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers("/auth/**","/actuator/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().authenticated()
             }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
         return http.build()
     }
 }
---
+++ src/main/kotlin/com/bav/airneisbackend/utilisateur/userside/restressource/InscriptionUtilisateurRestRessource.kt
@@ -0,0 +1,7 @@
+package com.bav.airneisbackend.utilisateur.userside.restressource
+
+data class InscriptionUtilisateurRestRessource(
+    val email : String,
+    val motDePasse : String,
+    val nom : String
+)
