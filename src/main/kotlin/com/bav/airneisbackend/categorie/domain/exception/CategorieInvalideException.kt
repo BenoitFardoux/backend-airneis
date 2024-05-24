e2b2a7a0918d076fc9917312ad058abe355c16da
valentin.leroux <Valentin.leroux@rtssolutions.fr>
2024-05-24 15:22:45 +0200

    feat(categorie): Ajout serverside

--- src/main/kotlin/com/bav/airneisbackend/categorie/domain/exception/CategorieInvalideException.kt
+++ src/main/kotlin/com/bav/airneisbackend/categorie/domain/exception/CategorieInvalideException.kt
@@ -1,3 +1,4 @@
 package com.bav.airneisbackend.categorie.domain.exception
 
-class CategorieInvalideException(override val description: String, val champs : List<String>) : CategorieException(description)
+class CategorieInvalideException(override val description: String, val champs : List<String>) :
+    CategorieException(description)
