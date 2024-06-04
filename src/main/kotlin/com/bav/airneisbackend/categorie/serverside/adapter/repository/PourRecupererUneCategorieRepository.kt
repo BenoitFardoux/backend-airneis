64faa2a1b6f7f16e80dd269797d683430b68980a
benoit.fardoux <benoit.fardoux@rtssolutions.fr>
2024-06-04 21:27:55 +0200

    fix(route get categorie) :  synchronisation du get dans le cas d'un produit supprimé

--- src/main/kotlin/com/bav/airneisbackend/categorie/serverside/adapter/repository/PourRecupererUneCategorieRepository.kt
+++ src/main/kotlin/com/bav/airneisbackend/categorie/serverside/adapter/repository/PourRecupererUneCategorieRepository.kt
@@ -5,15 +5,33 @@ import com.bav.airneisbackend.categorie.domain.model.Categorie
 import com.bav.airneisbackend.categorie.domain.port.serverside.categorie.PourRecupererUneCategorie
 import com.bav.airneisbackend.categorie.serverside.adapter.mongodb.repository.MongoDbCategorieRepository
 import com.bav.airneisbackend.categorie.serverside.mapper.CategorieMapper.toCategorie
+import com.bav.airneisbackend.produit.serverside.adapter.mongodb.repository.MongoDbProduitRepository
 import org.springframework.stereotype.Repository
 
 
 @Repository
-class PourRecupererUneCategorieRepository(val mongoDbCategorieRepository: MongoDbCategorieRepository) : PourRecupererUneCategorie {
+class PourRecupererUneCategorieRepository(
+    val mongoDbCategorieRepository: MongoDbCategorieRepository,
+    val mongoDbProduitRepository: MongoDbProduitRepository
+) :
+    PourRecupererUneCategorie {
     override fun invoke(id: String): Categorie {
         if (!mongoDbCategorieRepository.existsById(id)) {
             throw CategorieNonTrouveeException(id)
         }
+        val categorie = mongoDbCategorieRepository.findById(id).get()
+        var bool = false
+        categorie.produits.map { item ->
+            mongoDbProduitRepository.existsById(item.id).let {
+                if (!it){
+                    categorie.produits.remove(item)
+                    bool = true
+                }
+            }
+        if (bool) mongoDbCategorieRepository.save(categorie)
+        }
+
+
         return mongoDbCategorieRepository.findById(id).get().toCategorie()
 
     }
--- src/main/kotlin/com/bav/airneisbackend/produit/serverside/adapter/repository/PourRecupererUnProduitRepository.kt
+++ src/main/kotlin/com/bav/airneisbackend/produit/serverside/adapter/repository/PourRecupererUnProduitRepository.kt
@@ -6,12 +6,13 @@ import com.bav.airneisbackend.produit.domain.port.serverside.produit.PourRecuper
 import com.bav.airneisbackend.produit.serverside.adapter.mongodb.repository.MongoDbProduitRepository
 import com.bav.airneisbackend.produit.serverside.mapper.ProduitMapper.toProduit
 import org.springframework.stereotype.Repository
-import kotlin.jvm.optionals.getOrNull
+import kotlin.jvm.optionals.getOrElse
 
 
 @Repository
 class PourRecupererUnProduitRepository(private val mongoDbProduitRepository: MongoDbProduitRepository) : PourRecupererProduitParId {
-    override fun invoke(id: String): Produit {
-        return mongoDbProduitRepository.findById(id).get().toProduit()
+    override operator fun invoke(id: String): Produit {
+        return mongoDbProduitRepository.findById(id).getOrElse { throw AucunProduitTrouveException("Produit $id non trouvé") }
+            .toProduit()
     }
 }
